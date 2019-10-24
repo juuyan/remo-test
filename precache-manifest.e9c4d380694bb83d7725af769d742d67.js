6435c1bec1215f9493dfe091e05d80ec6d5942c1
Zhu Yan <why.eh.n@gmail.com>
2019-10-24 15:40:17 +0800

    Updates

---
+++ precache-manifest.e9c4d380694bb83d7725af769d742d67.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0203778eaf149a45925e8e06677351a",
+    "url": "/remo-test/index.html"
+  },
+  {
+    "revision": "fa76cad928398f6485d9",
+    "url": "/remo-test/static/css/main.07e6bac6.chunk.css"
+  },
+  {
+    "revision": "ecc6420ca82e5ad3543f",
+    "url": "/remo-test/static/js/2.6911cdcc.chunk.js"
+  },
+  {
+    "revision": "fa76cad928398f6485d9",
+    "url": "/remo-test/static/js/main.cf44dd82.chunk.js"
+  },
+  {
+    "revision": "3747012d12ba66e7d69f",
+    "url": "/remo-test/static/js/runtime-main.6e58ec39.js"
+  },
+  {
+    "revision": "0bffa0bc5873f47bc7d3c50b4d681768",
+    "url": "/remo-test/static/media/screenshort.0bffa0bc.png"
+  }
+]);
